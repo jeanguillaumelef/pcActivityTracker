10060f755f94093af5d04b6ab4004a744c6c0c51
jeanguillaumelef <jeanguillaumelefebvre@gmail.com>
2024-01-30 15:38:20 +0100

    POC code to get active window

---
+++ WindowsActivityTracker/TestWindowsActivityTracker/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace TestWindowsActivityTracker
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ WindowsActivityTracker/WindowsActivityTracker/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+class Program
+{
+    [DllImport("user32.dll")]
+    private static extern IntPtr GetForegroundWindow();
+
+    [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+    private static extern int GetWindowText(IntPtr hWnd, string lpString, int nMaxCount);
+
+    static void Main()
+    {
+        while (true)
+        {
+            IntPtr foregroundWindow = GetForegroundWindow();
+            const int maxPathSize = 256;
+            string windowTitle = new string(' ', maxPathSize);
+
+            GetWindowText(foregroundWindow, windowTitle, maxPathSize);
+            windowTitle = windowTitle.Split('\\').Last();
+
+            Console.WriteLine("Active Window: " + windowTitle);
+
+            // Sleep for a short interval
+            System.Threading.Thread.Sleep(1000);
+        }
+    }
+}
+
